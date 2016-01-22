94a13e7d84fb121bcb5919a8b78f5e852616470a
Christopher Yung <cyung10892@gmail.com>
2016-01-22 13:29:50 -0500

    add api subdomain

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var subdomain = require('express-subdomain');
 var routes = require('./routes/index');
+var api = require('./routes/api');
 var users = require('./routes/users');
 var turtle = require('./routes/turtle');
 var mongoose = require('mongoose');
@@ -18,16 +19,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
 app.use('/turtle', turtle);
+app.use(subdomain('api', api));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
