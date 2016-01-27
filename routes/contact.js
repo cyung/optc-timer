5aca61c197b29ae194a87b66c05723ac0618ce37
Christopher Yung <cyung10892@gmail.com>
2016-01-27 10:38:37 -0500

    update contact form to include game version

--- routes/contact.js
+++ routes/contact.js
@@ -13,11 +13,13 @@ var transporter = nodemailer.createTransport(path);
 router.post('/', function(req, res, next) {
   console.log('req.body =', req.body);
   var description = 'From: ' + req.body.name + ' <' + req.body.email + '>';
-  description += '\n\n' + req.body.description;
+  description += '\n\nDescription:\n' + req.body.description;
 
   if (req.body.subject === 'Incorrect Turtle Time') {
     if ('correctBefore' in req.body)
       description += '\n\nWas it correct before? ' + req.body.correctBefore;
+    if ('gameVersion' in req.body)
+      description += '\nGame Version: ' + req.body.gameVersion;
     if ('browser' in req.body)
       description += '\nBrowser: ' + req.body.browser;
     if ('timeZone' in req.body)
