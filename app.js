14b5ce3293790b574a055bf1caf54242323789c1
Christopher Yung <cyung10892@gmail.com>
2016-05-31 20:59:39 -0400

    allow cross origin requests

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.use(
     .set('prerenderToken', 'YOUR_TOKEN')
   );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // app.use('/', routes);
 app.use('/api', api);
 app.use('/contact', contact);
